3b37906d59fbafdd1de8145397f2857f0a916832
Stanislav B <stanislav@mail.fake>
2013-01-10 10:46:06 +0100

    Added Nagios SNMP location check and memcached scripts to get values.

---
+++ memcached/memcached_debug_get.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+# Authors: Stanislav Blokhin
+
+import memcache
+
+# Config
+# Either hostname/IP or UNIX socket
+memcached_address=['unix:/var/run/memcached/memcached.sock']
+
+# We need at least one argument
+if len(sys.argv) < 2:
+	print("usage: %s key" % sys.argv[0])
+        sys.exit(1)
+
+# Index 0 is script name, index 1 is first argument
+key = sys.argv[1]
+
+mc = memcache.Client(memcached_address, debug=1)
+print(mc.get(key))
---
+++ memcached/memcached_get.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+# Authors: Stanislav Blokhin
+
+import memcache
+import sys
+
+# Config
+# Either hostname/IP or UNIX socket
+memcached_address=['unix:/var/run/memcached/memcached.sock']
+
+# We need at least one argument
+if len(sys.argv) < 2:
+	print("%s needs an argument" % sys.argv[0])
+	sys.exit(1)
+
+mc = memcache.Client(memcached_address, debug=0)
+# Index 0 is script name, index 1 is first argument
+key = sys.argv[1]
+ret = mc.get(key)
+
+# If no memcached response
+if ret is None:
+	print("Unknown")
+	sys.exit(0)
+
+# All went well
+print(ret)
+sys.exit(0)
